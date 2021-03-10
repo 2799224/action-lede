9c91da2e8dd732c60f25bb904d11929c716a6921
2799224 <80330575+2799224@users.noreply.github.com>
2021-03-10 15:13:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy1
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/luci-theme-rosy
