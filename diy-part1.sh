5885e2888bb64cbe3434909a0b2044610450b4b5
2799224 <80330575+2799224@users.noreply.github.com>
2021-03-14 20:43:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -43,7 +43,7 @@
 #git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 #git clone https://github.com/tsl0922/ttyd.git package/ttyd
 
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+#@sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
+#@git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 #git clone https://github.com/tsl0922/ttyd.git package/ttyd
 
