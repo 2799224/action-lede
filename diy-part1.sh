e45ddbe2785e9094465145218936966cd3597b6b
2799224 <80330575+2799224@users.noreply.github.com>
2021-03-15 12:01:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -47,3 +47,5 @@
 #@git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 #git clone https://github.com/tsl0922/ttyd.git package/ttyd
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
